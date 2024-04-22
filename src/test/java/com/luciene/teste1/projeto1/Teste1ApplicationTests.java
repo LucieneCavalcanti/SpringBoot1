a45640e6a8c805826b9680fd60022288c06dbd2c
LucieneCavalcanti <lucienecavalcanti@live.com>
2024-04-22 14:26:32 -0300

    Início do projeto

---
+++ src/test/java/com/luciene/teste1/projeto1/Teste1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luciene.teste1.projeto1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Teste1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
